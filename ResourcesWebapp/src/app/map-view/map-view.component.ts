47f9be50e5b8f2f466c12b9e696ed36d13b869e7
hermana <aeherman@ualberta.ca>
2022-11-28 23:32:10 -0600

    adding map

--- ResourcesWebapp/src/app/map-view/map-view.component.ts
+++ ResourcesWebapp/src/app/map-view/map-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map-view',
@@ -7,9 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapViewComponent implements OnInit {
 
+  apiLoaded: Observable<boolean>;
+  apiUrl: string =
+    'https://maps.googleapis.com/maps/api/js?key=' + environment.GOOGLE_API_KEY;
+
+  resourceLocations = [] //FIXME: create a model to type this
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+
+
+
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { environment } from 'src/environments/environment';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable, of } from 'rxjs';
+// import { catchError, map } from 'rxjs/operators';
+// import { ServiceDisruptionsService } from 'src/app/core/services/service-disruptions.service';
+// import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+// import { ServiceDisruption } from 'src/app/core/models/service-disruption';
+
+// @Component({
+//   selector: 'app-disruptions-map',
+//   templateUrl: './map.component.html',
+//   styleUrls: ['./map.component.scss'],
+// })
+export class MapComponent implements OnInit {
+  apiLoaded: Observable<boolean>;
+  apiUrl: string =
+    'https://maps.googleapis.com/maps/api/js?key=' + environment.GOOGLE_API_KEY;
+
+  disruptions: ServiceDisruption[] = [];
+  selectedDisruption?: ServiceDisruption;
+
+  @ViewChild('googleMap') map?: GoogleMap;
+  @ViewChild(MapInfoWindow) infoWindow?: MapInfoWindow;
+
+  mapOptions: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: false,
+    restriction: {
+      latLngBounds: {
+        north: 62,
+        south: 46,
+        west: -115,
+        east: -95,
+      },
+    },
+  };
+
+  constructor(
+    private http: HttpClient,
+    private disruptionsService: ServiceDisruptionsService
+  ) {
+    // Lazy load google maps API
+    // TODO: Move to root service
+    this.apiLoaded = http.jsonp(this.apiUrl, 'callback').pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+
+  ngOnInit(): void {
+    this.disruptionsService.getDisruptions().subscribe({
+      next: (result) => {
+        this.disruptions = result;
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    let communityName = marker.getTitle();
+    this.selectedDisruption = this.disruptions.find(
+      (d) => d.community_name == communityName
+    );
+    this.infoWindow?.open(marker);
+  }
 }
