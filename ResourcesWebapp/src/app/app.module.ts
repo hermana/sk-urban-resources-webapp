76a5132c4575e3557bd2ec2403c3b4d7eb3c927d
hermana <aeherman@ualberta.ca>
2023-01-17 22:50:01 -0600

    removed boostrap for now

--- ResourcesWebapp/src/app/app.module.ts
+++ ResourcesWebapp/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule,
     GoogleMapsModule,
     HttpClientJsonpModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     HttpClient, 
